bb723f5a78cd15e53bf3bf5d69579a3234a06780
pactera <pactera@192.168.0.103>
2017-10-24 09:22:29 +0800

    update

--- src/main/java/com/xsintech/controller/IndexController.java
+++ src/main/java/com/xsintech/controller/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-	@RequestMapping(value = "/login")
+	@RequestMapping(value = "/")
 	public String login() {
 		return "login";
 	}
